6cb983cc216e5b55955109f326c10d55c794064c
lucKulke <lucaskulke98@gmail.com>
2023-08-06 23:41:24 +0200

    foundations for implementing chat gpt

--- webapp/main.rb
+++ webapp/main.rb
@@ -11,7 +11,6 @@ require "sinatra/cross_origin"
 configure do
   enable :sessions
   set :session_secret, ENV['TANDEM_SESSION']
-
 end
 
 Aws.config.update(
@@ -19,8 +18,15 @@ Aws.config.update(
   credentials: Aws::Credentials.new(ENV["IAM_USER_TEST_ACCESS_KEY"], ENV["IAM_USER_TEST_SECRET_ACCESS_KEY"])
 )
 
+OpenAI.configure do |config|
+  config.access_token = 'YOUR_API_KEY'
+end
+
+CHAT_GPT_SYSTEM_MESSAGE = "You are a helpful assistant"
 
 # This endpoint generates a pre-signed URL and includes the filename in the response
+user = User.new
+
 post '/get_upload_url' do
   s3 = Aws::S3::Resource.new
   filename = "recording_#{SecureRandom.uuid}.wav" # Generate a unique filename
@@ -33,10 +39,44 @@ post '/get_upload_url' do
   { url: url, filename: filename }.to_json
 end
 
+post '/get_visper_result' do
+
+# get visper text
+
+# store text in user obj
+text = 'How are you?'
+# send text to chat gpt
+response = generate_response(conversation)
+# receive answer (text) from chat gpt
+
+# store text in user obj
+
+# send text to "text to speach AI"
+end
+
+post '/get_result_audio_file_reference' do 
+
+  # make s3 request for presigned url for downloading audio file
+  # send back to client
+
+  # store in user obj
+
+end
+
+
 get "/" do 
   erb :home
 end
 
 
+def generate_response_chat_gpt(conversation)
+  response = OpenAI::ChatCompletion.create(
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: conversation },
+               {role: 'system', content: CHAT_GPT_SYSTEM_MESSAGE}],
+    temperature: 0.7
+  )
 
+  response.choices[0].message.content
+end
 
