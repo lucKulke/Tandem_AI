5d065f9212c94c0d9bf7668ff18198c2549fdfca
lucKulke <lucaskulke98@gmail.com>
2023-08-09 14:44:08 +0200

    cleaned data management code and implemented uuids

--- webapp/conversation.rb
+++ webapp/conversation.rb
@@ -20,33 +20,33 @@ class Conversation
 
   def speech_recognition_transcription_ai_data(audio_file_key, output_text, timestamp_input, timestamp_output, healthcode)
     @speech_recognition_transcription_ai[self.iteration] = {
-      speech_recognition_transcription_audio_file_key: audio_file_key,
-      speech_recognition_transcription_output: output_text,
-      speech_recognition_transcription_timestamp_input: timestamp_input,
-      speech_recognition_transcription_timestamp_output: timestamp_output,
-      speech_recognition_transcription_healthcode: healthcode
+      audio_file_key: audio_file_key,
+      output_text: output_text,
+      timestamp_input: timestamp_input,
+      timestamp_output: timestamp_output,
+      healthcode: healthcode
     }
       
   end
 
   def language_processing_ai_data(input_text, output_text, timestamp_input, timestamp_output, healthcode)
     @language_processing_ai[self.iteration] = {
-      language_processing_input: input_text,
-      language_processing_output: output_text,
-      language_processing_timestamp_input: timestamp_input,
-      language_processing_timestamp_output:timestamp_output,
-      language_processing_healthcode: healthcode
+      input_text: input_text,
+      output_text: output_text,
+      timestamp_input: timestamp_input,
+      timestamp_output:timestamp_output,
+      healthcode: healthcode
     }
 
   end
 
   def voice_generator_ai_data(input_text, audio_file_key, timestamp_input, timestamp_output, healthcode)
     @voice_generator_ai[self.iteration] = {
-      voice_generator_input: input_text,
-      voice_generator_audio_file_key: audio_file_key,
-      voice_generator_timestamp_input: timestamp_input,
-      voice_generator_timestamp_output: timestamp_output,
-      voice_generator_healthcode: healthcode
+      input_text: input_text,
+      audio_file_key: audio_file_key,
+      timestamp_input: timestamp_input,
+      timestamp_output: timestamp_output,
+      healthcode: healthcode
     }
   end
   
--- webapp/user.rb
+++ webapp/user.rb
@@ -7,8 +7,8 @@ class User
 
   def initialize(first_name, surname, email)
     @db_connection = DatabaseConnection.new.establish
-    @user_id = load_user_id(first_name, surname, email)
-    @conversations = load_conversation_id_and_text(user_id)
+    @user_id = load_user_data(first_name, surname, email)
+    @conversations = {}
     @current_conversation_id = nil
     @current_conversation = nil
   end
@@ -28,6 +28,22 @@ class User
 
   private 
 
+  def load_user_data(first_name, surname, email)
+    user_id = search_user_id(first_name, surname, email)
+    if user_id.nil?
+      user_id = create_user_in_db(first_name, surname, email)
+    else
+      self.conversations = load_conversations(user_id)
+    end
+    user_id
+  end
+
+  def create_user_in_db(first_name, surname, email)
+    result = db_connection.query("INSERT INTO users (user_id, first_name, surname, email)
+                        VALUES (UUID(), '#{first_name}', '#{surname}', '#{email}');
+                        SELECT LAST_INSERT_ID() AS user_uuid;")
+    result.first['user_uuid'] # returns user_id
+  end
 
   def search_user_id(first_name, surname, email)
     result = db_connection.query("SELECT user_id
@@ -37,8 +53,7 @@ class User
     result.each{ |row| return row['user_id'] }
   end
 
-
-  def load_conversation_id_and_text(user_id)
+  def load_conversations(user_id)
     return_format = {}
     result = db_connection.query("SELECT conversation_id, conversation
                         FROM conversations
@@ -50,34 +65,39 @@ class User
     return_format
   end
 
-  def load_conversation_text(conversation_id)
-    self.conversations[conversation_id]
-  end
-
   def upload_conversation_to_db(conversation_id)
-    
     # loads each iteration in each ai table
     (1..current_conversation.iteration).each do |iteration|
+      iteration_id = db_connection.query('SELECT UUID();')
       speech_recognition_transcription_ai_row = current_conversation.speech_recognition_transcription_ai_data[iteration]
       db_connection.query("INSERT INTO speech_recognition_transcription_ai
-                          (user_id, iteration, conversation_id, input_audio_file_ref, output, timestamp_input, timestamp_output, health_code)
-                          VALUES(#{user_id}, #{iteration}, #{conversation_id}, #{speech_recognition_transcription_ai_row[:speech_recognition_transcription_audio_file_key]},
-                          #{speech_recognition_transcription_ai_row[:speech_recognition_transcription_output]}, #{speech_recognition_transcription_ai_row[:speech_recognition_transcription_timestamp_input]}, 
-                          #{speech_recognition_transcription_ai_row[:speech_recognition_transcription_timestamp_output]}, #{speech_recognition_transcription_ai_row[:speech_recognition_transcription_healthcode]})")
+                          (user_id, iteration_id, conversation_id, audio_file_key, output_text, timestamp_input, timestamp_output, health_code)
+                          VALUES(#{user_id}, #{iteration_id}, #{conversation_id}, 
+                          #{speech_recognition_transcription_ai_row[:audio_file_key]},
+                          #{speech_recognition_transcription_ai_row[:output_text]},
+                          #{speech_recognition_transcription_ai_row[:timestamp_input]}, 
+                          #{speech_recognition_transcription_ai_row[:timestamp_output]},
+                          #{speech_recognition_transcription_ai_row[:healthcode]});")
       
       language_processing_ai_row = current_conversation.language_processing_ai_data[iteration]
       db_connection.query("INSERT INTO language_processing_ai
-                          (user_id, iteration, conversation_id, input_audio_file_ref, output, timestamp_input, timestamp_output, health_code)
-                          VALUES(#{user_id}, #{iteration}, #{conversation_id}, #{language_processing_ai_row[:language_processing_input]},
-                          #{language_processing_ai_row[:language_processing_output]}, #{language_processing_ai_row[:language_processing_timestamp_input]}, 
-                          #{language_processing_ai_row[:language_processing_timestamp_output]}, #{language_processing_ai_row[:language_processing_healthcode]})")
+                          (user_id, iteration_id, conversation_id, input_text, output_text, timestamp_input, timestamp_output, health_code)
+                          VALUES(#{user_id}, #{iteration_id}, #{conversation_id},
+                          #{language_processing_ai_row[:input_text]},
+                          #{language_processing_ai_row[:output_text]},
+                          #{language_processing_ai_row[:timestamp_input]}, 
+                          #{language_processing_ai_row[:timestamp_output]},
+                          #{language_processing_ai_row[:healthcode]});")
                         
       voice_generator_ai_row = current_conversation.voice_generator_ai_data[iteration]
       db_connection.query("INSERT INTO voice_generator_ai
-                          (user_id, iteration, conversation_id, input_audio_file_ref, output, timestamp_input, timestamp_output, health_code)
-                          VALUES(#{user_id}, #{iteration}, #{conversation_id}, #{voice_generator_ai_row[:voice_generator_input]},
-                          #{voice_generator_ai_row[:voice_generator_audio_file_key]}, #{voice_generator_ai_row[:voice_generator_timestamp_input]}, 
-                          #{voice_generator_ai_row[:voice_generator_timestamp_output]}, #{voice_generator_ai_row[:voice_generator_healthcode]})")
+                          (user_id, iteration_id, conversation_id, audio_file_key, output_text, timestamp_input, timestamp_output, health_code)
+                          VALUES(#{user_id}, #{iteration_id}, #{conversation_id},
+                          #{voice_generator_ai_row[:input_text]},
+                          #{voice_generator_ai_row[:audio_file_key]},
+                          #{voice_generator_ai_row[:timestamp_input]}, 
+                          #{voice_generator_ai_row[:timestamp_output]},
+                          #{voice_generator_ai_row[:healthcode]});")
 
       # update conversation table
       db_connection.query("UPDATE conversations SET conversation = #{current_conversation.conversation_text} WHERE conversation_id = #{conversation_id}")
