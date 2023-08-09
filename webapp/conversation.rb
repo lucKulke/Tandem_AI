a1a28d5aa50c5238385b5611776def65d8a6c29d
lucKulke <lucaskulke98@gmail.com>
2023-08-09 21:52:37 +0200

    changed iteration data handling

--- webapp/conversation.rb
+++ webapp/conversation.rb
@@ -1,17 +1,17 @@
 class Conversation
-  attr_accessor :iteration, :speech_recognition_transcription_ai, :language_processing_ai, :voice_generator_ai
+  attr_accessor :iteration, :speech_recognition_transcription_ai, :language_processing_ai, :voice_generator_ai, :conversation_text
   
   def initialize(conversation_text)
     @conversation_text = conversation_text 
-    @iteration = 0
     @speech_recognition_transcription_ai = {}
     @language_processing_ai = {}
     @voice_generator_ai = {}
   end
 
-  def start_iteration
-    self.iteration += 1
+  def data
+    [self.speech_recognition_transcription_ai, self.language_processing_ai, self.voice_generator_ai]
   end
+
 
   def end_iteration
     self.conversation_text += "user: #{language_processing_ai[self.iteration][:input]}\nai: #{language_processing_ai[self.iteration][:output]}\n\n"
@@ -19,7 +19,7 @@ class Conversation
 
 
   def speech_recognition_transcription_ai_data(audio_file_key, output_text, timestamp_input, timestamp_output, healthcode)
-    @speech_recognition_transcription_ai[self.iteration] = {
+    @speech_recognition_transcription_ai = {
       audio_file_key: audio_file_key,
       output_text: output_text,
       timestamp_input: timestamp_input,
@@ -30,7 +30,7 @@ class Conversation
   end
 
   def language_processing_ai_data(input_text, output_text, timestamp_input, timestamp_output, healthcode)
-    @language_processing_ai[self.iteration] = {
+    @language_processing_ai = {
       input_text: input_text,
       output_text: output_text,
       timestamp_input: timestamp_input,
@@ -41,7 +41,7 @@ class Conversation
   end
 
   def voice_generator_ai_data(input_text, audio_file_key, timestamp_input, timestamp_output, healthcode)
-    @voice_generator_ai[self.iteration] = {
+    @voice_generator_ai = {
       input_text: input_text,
       audio_file_key: audio_file_key,
       timestamp_input: timestamp_input,
